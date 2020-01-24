484c3beddafbaa98199f4c9c4430d6f28b434353
houseferroccite <56713379+houseferroccite@users.noreply.github.com>
2020-01-24 18:19:57 +0300

    Update: - Get_Document: Create document with Т_Клиент

--- Application Data/Functional/Get_Documents_inf.Designer.cs
+++ Application Data/Functional/Get_Documents_inf.Designer.cs
@@ -30,25 +30,25 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Get_Documents_inf));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox_Klient = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.grid_info_all = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid_info_all)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.DodgerBlue;
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.comboBox2);
+            this.panel1.Controls.Add(this.comboBox_Klient);
             this.panel1.Controls.Add(this.dateTimePicker1);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label3);
@@ -59,44 +59,30 @@
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
             // 
-            // panel2
+            // pictureBox1
             // 
-            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(561, 113);
-            this.panel2.TabIndex = 0;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(239, 136);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // comboBox_Klient
             // 
-            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 25F);
-            this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(3, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(559, 113);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Документы о деятельности \r\nкомпании";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.comboBox_Klient.FormattingEnabled = true;
+            this.comboBox_Klient.Location = new System.Drawing.Point(20, 286);
+            this.comboBox_Klient.Name = "comboBox_Klient";
+            this.comboBox_Klient.Size = new System.Drawing.Size(198, 25);
+            this.comboBox_Klient.TabIndex = 2;
             // 
-            // dataGridView1
+            // dateTimePicker1
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 113);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 27;
-            this.dataGridView1.Size = new System.Drawing.Size(562, 338);
-            this.dataGridView1.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 194);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(171, 19);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Просмотр Договоров от:\r\n";
+            this.dateTimePicker1.Location = new System.Drawing.Point(20, 221);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
+            this.dateTimePicker1.TabIndex = 1;
             // 
             // label5
             // 
@@ -107,46 +93,61 @@
             this.label5.TabIndex = 0;
             this.label5.Text = "Просмотр данных о Клиенте:";
             // 
-            // dateTimePicker1
+            // label3
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(20, 221);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 25);
-            this.dateTimePicker1.TabIndex = 1;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 194);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Просмотр Договоров от:\r\n";
             // 
-            // comboBox2
+            // panel2
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(20, 286);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(198, 25);
-            this.comboBox2.TabIndex = 2;
+            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(561, 139);
+            this.panel2.TabIndex = 0;
             // 
-            // pictureBox1
+            // label1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(239, 113);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semilight", 25F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(559, 131);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Документы о деятельности \r\nкомпании";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // grid_info_all
+            // 
+            this.grid_info_all.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid_info_all.Location = new System.Drawing.Point(0, 134);
+            this.grid_info_all.Name = "grid_info_all";
+            this.grid_info_all.RowTemplate.Height = 27;
+            this.grid_info_all.Size = new System.Drawing.Size(562, 317);
+            this.grid_info_all.TabIndex = 1;
             // 
             // Get_Documents_inf
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.grid_info_all);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Get_Documents_inf";
             this.Text = "Документация";
+            this.Load += new System.EventHandler(this.Get_Documents_inf_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid_info_all)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -156,9 +157,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView grid_info_all;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox_Klient;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
--- Application Data/Functional/Get_Documents_inf.cs
+++ Application Data/Functional/Get_Documents_inf.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Course_project;
+using Course_project.Method_s_for_settings_GridView;
+using Course_project.Методы;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,10 +19,23 @@ namespace course1._0.Application_Data.Functional
         {
             InitializeComponent();
         }
-
+        DataTable dt,dt2;
         private void Panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void Get_Documents_inf_Load(object sender, EventArgs e)
+        {
+            GridViewStyles.ChancheViewGrid(grid_info_all);
+            BindingSource bs_login = new BindingSource();
+            BindingSource bs_KL = new BindingSource();
+            bs_login.DataSource = dt;
+            bs_KL.DataSource = dt2;
+            dt = DB.LoadTable("SELECT Т_Клиент.ID_Клиента as aa, Т_Клиент.ФИО as bb FROM Т_Клиент", "Т_Клиент");
+            dt2 = DB.LoadTable($"SELECT * FROM Т_Клиент  WHERE ФИО = {comboBox_Klient.Text}", "Т_Клиент");
+            grid_info_all.DataSource = dt2;
+            QuicklyChangeDB.ComboSettings(dt, "ФИО", "ID_Клиента", comboBox_Klient);
         }
     }
 }
